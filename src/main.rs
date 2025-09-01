79521eb8fda7befd4da508c1d130c86b348438a6
tris <68403260+tris5572@users.noreply.github.com>
2025-09-02 06:51:40 +0900

    feat: エラー処理を anyhow へ移行

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
+use anyhow::Result;
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<()> {
     let content = get_data().await?;
     println!("{}", &content);
 
     Ok(())
 }
 
-async fn get_data() -> Result<String, Box<dyn std::error::Error>> {
+async fn get_data() -> Result<String> {
     let response = reqwest::get("https://httpbin.org/ip").await?;
     let text = response.text().await?;
 
