2196360ddbf7dc5e67639ae6dbb36796d298f988
tris <68403260+tris5572@users.noreply.github.com>
2025-09-02 06:29:18 +0900

    feat: データ取得を暫定実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let content = get_data().await?;
+    println!("{}", &content);
+
+    Ok(())
+}
+
+async fn get_data() -> Result<String, Box<dyn std::error::Error>> {
+    let response = reqwest::get("https://httpbin.org/ip").await?;
+    let text = response.text().await?;
+
+    Ok(text)
 }
