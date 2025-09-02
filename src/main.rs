3f57aa1e99154dfea1cde35d8701a65a3371b37d
tris <68403260+tris5572@users.noreply.github.com>
2025-09-03 05:00:23 +0900

    refactor: データを取得する文字列を引数で渡すように変更

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,15 @@ use anyhow::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let content = get_data().await?;
+    let content = get_data("https://httpbin.org/ip").await?;
     println!("{}", &content);
 
     Ok(())
 }
 
-async fn get_data() -> Result<String> {
-    let response = reqwest::get("https://httpbin.org/ip").await?;
+/// 指定した url からデータを取得し、文字列を返す。
+async fn get_data(url: &str) -> Result<String> {
+    let response = reqwest::get(url).await?;
     let text = response.text().await?;
 
     Ok(text)
