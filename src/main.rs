6b50dea9a73b92a3a70555cee48304bce4aa7188
tris <68403260+tris5572@users.noreply.github.com>
2025-09-03 05:01:07 +0900

    refactor: 分かりやすくするために関数名を変更

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@ use anyhow::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let content = get_data("https://httpbin.org/ip").await?;
+    let content = get_data_from_url("https://httpbin.org/ip").await?;
     println!("{}", &content);
 
     Ok(())
 }
 
 /// 指定した url からデータを取得し、文字列を返す。
-async fn get_data(url: &str) -> Result<String> {
+async fn get_data_from_url(url: &str) -> Result<String> {
     let response = reqwest::get(url).await?;
     let text = response.text().await?;
 
