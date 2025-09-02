dab8ed57b662b76d44d009543637a9d0bd5935ed
tris <68403260+tris5572@users.noreply.github.com>
2025-09-03 05:24:00 +0900

    feat: ファイル保存処理を実装

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use anyhow::Result;
 #[tokio::main]
 async fn main() -> Result<()> {
     let content = get_data_from_url("https://httpbin.org/ip").await?;
+
+    save_to_file("output.txt", &content).await?;
     println!("{}", &content);
 
     Ok(())
@@ -14,4 +16,10 @@ async fn get_data_from_url(url: &str) -> Result<String> {
     let text = response.text().await?;
 
     Ok(text)
+}
+
+/// 指定した文字列をカレントディレクトリにファイルとして保存する。
+async fn save_to_file(filename: &str, content: &str) -> Result<()> {
+    tokio::fs::write(filename, content).await?;
+    Ok(())
 }
