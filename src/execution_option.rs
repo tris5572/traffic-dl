e090b7ee06dc767653da2a5183b53c84949f249a
tris <68403260+tris5572@users.noreply.github.com>
2025-09-12 04:57:11 +0900

    feat: 取得間隔の指定方法を変更

--- src/execution_option.rs
+++ src/execution_option.rs
@@ -21,6 +21,17 @@ impl ExecutionOption {
         let dt = datetime::parse(&args.date)
             .with_context(|| format!("{} を日時指定として解釈不能", args.date))?;
 
+        // 取得間隔
+        // - 未指定時は1時間ごとのデータのみを取得
+        // - `--5m` 指定時は、5分間ごとのデータのみを取得
+        // - `--1h` と `--5m` の両方指定時は、両方のデータを取得
+        let h1 = if args.h1.is_none() && args.m5.is_some() {
+            false
+        } else {
+            true
+        };
+        let m5 = if args.m5.is_some() { true } else { false };
+
         // TODO: 後で実装する
         // 取得対象のセンサーの種類である常設トラカンとCCTVトラカンを設定する
         // 基本的には両方とも対象とするが、片方のみが実行時に指定された場合はそちらのみを対象にする。
@@ -29,8 +40,8 @@ impl ExecutionOption {
 
         let execution_option = ExecutionOption {
             datetime: dt,
-            interval_h1: args.h1,
-            interval_m5: args.m5,
+            interval_h1: h1,
+            interval_m5: m5,
             type_permanent: type_permanent,
             type_cctv: type_cctv,
         };
@@ -47,8 +58,8 @@ mod execute_option_from_args_test {
     fn default_args() -> Cli {
         Cli {
             date: "20250102".to_string(),
-            h1: true,
-            m5: false,
+            h1: Some(true),
+            m5: None,
             permanent: None,
             cctv: None,
         }
@@ -87,6 +98,55 @@ mod execute_option_from_args_test {
 
             args.date = "".into();
             assert!(ExecutionOption::from_args(&args).is_err());
+        }
+    }
+
+    #[cfg(test)]
+    mod 間隔指定 {
+        use super::*;
+
+        #[test]
+        fn unspecified() {
+            let mut args = default_args();
+            args.h1 = None;
+            args.m5 = None;
+            let result = ExecutionOption::from_args(&args).unwrap();
+
+            assert!(result.interval_h1);
+            assert!(!result.interval_m5);
+        }
+
+        #[test]
+        fn only_1h() {
+            let mut args = default_args();
+            args.h1 = Some(true);
+            args.m5 = None;
+            let result = ExecutionOption::from_args(&args).unwrap();
+
+            assert!(result.interval_h1);
+            assert!(!result.interval_m5);
+        }
+
+        #[test]
+        fn only_5m() {
+            let mut args = default_args();
+            args.h1 = None;
+            args.m5 = Some(true);
+            let result = ExecutionOption::from_args(&args).unwrap();
+
+            assert!(!result.interval_h1);
+            assert!(result.interval_m5);
+        }
+
+        #[test]
+        fn both() {
+            let mut args = default_args();
+            args.h1 = Some(true);
+            args.m5 = Some(true);
+            let result = ExecutionOption::from_args(&args).unwrap();
+
+            assert!(result.interval_h1);
+            assert!(result.interval_m5);
         }
     }
 }
--- src/types.rs
+++ src/types.rs
@@ -5,12 +5,13 @@ use clap::Parser;
 pub struct Cli {
     /// YYYYMMDDフォーマットの日付
     pub date: String,
+
     /// 1時間ごとのデータを取得 (デフォルト)
-    #[arg(long = "1h", default_value_t = true)]
-    pub h1: bool,
-    /// 5分間隔のデータを取得。この指定時、1時間ごとのデータ(--1h)を取得しない
-    #[arg(long = "5m", conflicts_with = "h1")]
-    pub m5: bool,
+    #[arg(long = "1h")]
+    pub h1: Option<bool>,
+    /// 5分間隔のデータを取得。これのみの指定時、1時間ごとのデータ(--1h)を取得しない
+    #[arg(long = "5m")]
+    pub m5: Option<bool>,
 
     /// 常設トラカンのみを取得対象とする
     #[arg(long = "permanent")]
